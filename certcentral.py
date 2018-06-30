8bdfc83f6a213e1ff7a9c7cce04bc4006afaeeb6
Alex Monk <krenair@gmail.com>
2018-07-01 00:52:05 +0100

    license
    Change-Id: Id73c058fc1ccb670c1c356b95d4bcaa72210183e

--- certcentral.py
+++ certcentral.py
@@ -1,5 +1,19 @@
 # Central certificates service
 # Alex Monk <krenair@gmail.com>, May/June 2018
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 This module is the main source code behind Wikimedia's central certificates service.
 A description of it can be found at https://phabricator.wikimedia.org/T194962

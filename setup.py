a06ab2ad54075ab5a04732a7686b372d2050945a
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-01-17 13:19:39 +0100

    CI: Run tests with minimum and latest dependencies
    Attempt to detect similar issues in the future with dependencies updates
    by running the tests against the minimum required versions and the
    latest ones
    
    Bug: T213820
    Change-Id: Ie77ce58df279aaff21002cc8d6ed01662cd932a4

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
+import os
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+install_requires = [
+    'acme >= 0.28.0',
+    'cryptography >= 2.3',
+    'dnspython >= 1.16.0',
+    'flask >= 1.0.2',
+    'josepy >= 1.1.0',
+    'pyOpenSSL >= 19.0.0',
+    'requests >= 2.20.0',
+    'pyyaml >= 3.13'
+]
 
 extras_require = {
     # Test dependencies
@@ -15,6 +27,8 @@ extras_require = {
 
 # Generate minimum dependencies
 extras_require['tests-min'] = [dep.replace('>=', '==') for dep in extras_require['tests']]
+if os.getenv('ACMECHIEF_MIN_DEPS', False):
+    install_requires = [dep.replace('>=', '==') for dep in install_requires]
 
 setuptools.setup(
     name="acme-chief",
@@ -36,15 +50,6 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ),
-    install_requires=[
-        'acme >= 0.28.0',
-        'cryptography >= 2.3',
-        'dnspython >= 1.16.0',
-        'flask >= 1.0.2',
-        'josepy >= 1.1.0',
-        'pyOpenSSL >= 19.0.0',
-        'requests >= 2.20.0',
-        'pyyaml >= 3.13'
-    ],
+    install_requires=install_requires,
     extras_require=extras_require
 )

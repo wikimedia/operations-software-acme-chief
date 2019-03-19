4d64bfb16dc3d82e89d35b2e889494c1aa125ce5
Alex Monk <krenair@gmail.com>
2019-03-19 02:45:19 +0000

    Release 0.13
    This release includes the following changes:
    * acme-chief: Ensure that the CN is part of the SNI list for certs config
    * acme-chief-api: Add support for puppet HTTP API search operation
    * acme-chief: Use relative symlinks for the live one
    
    Bug: T218685
    Bug: T218418
    Bug: T207295
    Change-Id: I51606f6d0ce737bd63a75d55052d2b5c8ad98813
    (cherry picked from commit 509944d54d3c506826c91a243c042577a0bdea55)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extras_require['tests-min'] = [dep.replace('>=', '==') for dep in extras_require
 
 setuptools.setup(
     name="acme-chief",
-    version="0.12",
+    version="0.13",
     author="Alex Monk",
     author_email="krenair@gmail.com",
     description="Python application to request certificates from ACME servers and distribute to authorised clients.",

0a69f530a4996bbbdfc45b78c09705a176d42514
Valentin Gutierrez <vgutierrez@wikimedia.org>
2019-08-02 11:18:03 +0700

    Release 0.20
    This release includes the following changes:
         * Save the certificate using the 3 save modes on issuance time
    
    Bug: T229096
    Change-Id: I7fa21304db7ee39d609cf60f3882845ef033181f

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ if os.getenv('ACMECHIEF_MIN_DEPS', False):
 
 setuptools.setup(
     name="acme-chief",
-    version="0.19",
+    version="0.20",
     author="Alex Monk",
     author_email="krenair@gmail.com",
     description="Python application to request certificates from ACME servers and distribute to authorised clients.",

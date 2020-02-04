6adda76dcef560a393c20f301efd72c1eb866098
Valentin Gutierrez <vgutierrez@wikimedia.org>
2020-02-04 12:29:08 +0000

    Release 0.22
    This release includes the following changes:
         * Support custom DNS server port
         * Fix newly reported pylint R1724 (no-else-continue)
    
    Bug: T240614
    Change-Id: I90d44f9df6adf9a11ac21466e2870558f5979a59

--- acme_chief/acme_chief.py
+++ acme_chief/acme_chief.py
@@ -903,7 +903,7 @@ def main():
     responsible for maintaining your configured certificates - creating dummy self-signed
     ones to start with, then having them replaced with ones from your ACME server. This does
     not provide the ACMEChief API.""")
-    parser.add_argument('--version', action='version', version='0.21')
+    parser.add_argument('--version', action='version', version='0.22')
     parser.parse_args()
     ACMEChief().run()
 

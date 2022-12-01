e53926f42c9b2fa7c59de0b77108afb676e28723
Valentin Gutierrez <vgutierrez@wikimedia.org>
2022-12-01 21:16:12 +0100

    Release 0.36
    This release includes the following changes:
     * Bump dependencies to target debian bullseye
    
    Bug: T321309
    Change-Id: I96956646703d18d3d3608ea0223ad0a0b18c6fe3
    (cherry picked from commit 6986477af600e112eed355f437d13c94c0678720)

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if os.getenv('ACMECHIEF_MIN_DEPS', False):
 
 setuptools.setup(
     name="acme-chief",
-    version="0.35",
+    version="0.36",
     author="Alex Monk",
     author_email="krenair@gmail.com",
     description="Python application to request certificates from ACME servers and distribute to authorised clients.",
